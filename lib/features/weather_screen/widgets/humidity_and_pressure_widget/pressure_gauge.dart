6f242c882263f557f18685a3bb54943e1dc53216
Slave-An <slavik.dobrordniy7@gmail.com>
2025-03-14 12:54:47 +0200

    Gauge for pressure widget

---
+++ lib/features/weather_screen/widgets/humidity_and_pressure_widget/pressure_gauge.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/features/weather_screen/widgets/humidity_and_pressure_widget/gauge_painter.dart';
+
+class PressureGauge extends StatelessWidget {
+  final int value;
+
+  const PressureGauge({super.key, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 120,
+      height: 120,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          CustomPaint(size: Size(120, 120), painter: GaugePainter()),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                value.toString(),
+                style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'гПа',
+                style: TextStyle(fontSize: 14, color: Colors.white70),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
