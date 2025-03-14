d86c969aae1f450c0ba5191f045b045961f7ec47
Slave-An <slavik.dobrordniy7@gmail.com>
2025-03-14 12:46:17 +0200

    Add router config

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/features/home_screen/view/home_screen.dart';
+import 'package:weather_app/router/router.dart';
 
 class WeatherApp extends StatelessWidget {
   const WeatherApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
+    final route = AppRouter();
+    return MaterialApp.router(
+      routerConfig: route.config(),
     );
   }
 }
