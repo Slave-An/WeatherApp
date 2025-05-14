673b65c6b72c4482a5ebd181df4a0e01d2943b7d
Slave-An <slavik.dobrordniy7@gmail.com>
2025-05-14 17:16:43 +0300

    Regenerated

--- lib/router/router.gr.dart
+++ lib/router/router.gr.dart
@@ -11,29 +11,63 @@ part of 'router.dart';
 
 abstract class _$AppRouter extends RootStackRouter {
   // ignore: unused_element
-  _$AppRouter({super.navigatorKey});
+  _$AppRouter();
 
   @override
   final Map<String, PageFactory> pagesMap = {
     HomeRoute.name: (routeData) {
+      final args = routeData.argsAs<HomeRouteArgs>(
+        orElse: () => const HomeRouteArgs(),
+      );
       return AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const HomeScreen(),
+        child: HomeScreen(key: args.key, initialPage: args.initialPage),
       );
-    }
+    },
+    WeatherListRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const WeatherListScreen(),
+      );
+    },
   };
 }
 
 /// generated route for
 /// [HomeScreen]
-class HomeRoute extends PageRouteInfo<void> {
-  const HomeRoute({List<PageRouteInfo>? children})
-      : super(
-          HomeRoute.name,
-          initialChildren: children,
-        );
+class HomeRoute extends PageRouteInfo<HomeRouteArgs> {
+  HomeRoute({Key? key, int initialPage = 0, List<PageRouteInfo>? children})
+    : super(
+        HomeRoute.name,
+        args: HomeRouteArgs(key: key, initialPage: initialPage),
+        initialChildren: children,
+      );
 
   static const String name = 'HomeRoute';
+
+  static const PageInfo<HomeRouteArgs> page = PageInfo<HomeRouteArgs>(name);
+}
+
+class HomeRouteArgs {
+  const HomeRouteArgs({this.key, this.initialPage = 0});
+
+  final Key? key;
+
+  final int initialPage;
+
+  @override
+  String toString() {
+    return 'HomeRouteArgs{key: $key, initialPage: $initialPage}';
+  }
+}
+
+/// generated route for
+/// [WeatherListScreen]
+class WeatherListRoute extends PageRouteInfo<void> {
+  const WeatherListRoute({List<PageRouteInfo>? children})
+    : super(WeatherListRoute.name, initialChildren: children);
+
+  static const String name = 'WeatherListRoute';
 
   static const PageInfo<void> page = PageInfo<void>(name);
 }
