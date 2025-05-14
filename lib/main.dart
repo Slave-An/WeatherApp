83b835805b8b90ece29757cbc721f98aa09bd6b6
Slave-An <slavik.dobrordniy7@gmail.com>
2025-05-14 17:33:14 +0300

    Add DI init and Hive init

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-
+import 'package:get_it/get_it.dart';
 import 'package:weather_app/app.dart';
+import 'package:weather_app/service/dependency_injection_service/dependency_injection_service.dart';
+import 'package:weather_app/service/hive_service/hive_service.dart';
 
 Future main() async {
   await dotenv.load(fileName: '.env');
 
+  DependencyInjectionService().ititDI();
+  await GetIt.I<HiveService>().initHive();
   runApp(const WeatherApp());
 }
