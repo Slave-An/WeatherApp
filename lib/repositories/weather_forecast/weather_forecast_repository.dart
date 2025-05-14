84b62db6f447192c11fe79b0dcb7a2d277e27020
Slave-An <slavik.dobrordniy7@gmail.com>
2025-05-14 17:18:27 +0300

    Change logic for List<Cities> and add logic for one city

--- lib/repositories/weather_forecast/weather_forecast_repository.dart
+++ lib/repositories/weather_forecast/weather_forecast_repository.dart
@@ -1,24 +1,38 @@
 import 'package:dio/dio.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:weather_app/repositories/weather_forecast/abstract_weather_forecast_repository.dart';
 import 'package:weather_app/repositories/weather_forecast/model/all_weather_forecast.dart';
 
 class WeatherForecastRepository implements AbstractWeatherForecastRepository {
+  WeatherForecastRepository({
+    required this.dio,
+    required this.apiKey,
+    required this.apiUrl,
+  });
+
+  final Dio dio;
+  final String apiKey;
+  final String apiUrl;
+
   @override
-  Future<AllWeatherForecast> getAllForecast(String city) async {
-    final Dio dio = Dio();
-    final String apiKey = dotenv.env['API_KEY']!;
-    final String apiUrl = dotenv.env['API_URL']!;
+  Future<List<AllWeatherForecast>> getAllForecast(List<String> city) async {
+    final List<AllWeatherForecast> data = [];
+    for (var i = 0; i < city.length; i++) {
+      final request = await dio.get(
+        apiUrl,
+        queryParameters: {'key': apiKey, 'q': city[i], 'days': 10},
+      );
+      data.add(AllWeatherForecast.fromJson(request.data));
+    }
+
+    return data;
+  }
+
+  @override
+  Future<AllWeatherForecast> getForecast(String city) async {
     final request = await dio.get(
       apiUrl,
-      queryParameters: {
-        'key': apiKey,
-        'q': city,
-        'days': 10,
-      },
+      queryParameters: {'key': apiKey, 'q': city, 'days': 10},
     );
-
-    final data = request.data;
-    return AllWeatherForecast.fromJson(data);
+    return AllWeatherForecast.fromJson(request.data);
   }
 }
