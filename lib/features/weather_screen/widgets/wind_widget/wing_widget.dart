5a99cbbc65cad99ee99a14f738a4d2165a9e0c45
Slave-An <slavik.dobrordniy7@gmail.com>
2025-05-14 17:35:28 +0300

    Changes

--- lib/features/weather_screen/widgets/wind_widget/wing_widget.dart
+++ lib/features/weather_screen/widgets/wind_widget/wing_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/features/weather_screen/widgets/base_container.dart';
-import 'package:weather_app/features/weather_screen/widgets/wind_widget/compas/compas.dart';
+import 'package:weather_app/features/weather_screen/widgets/wind_widget/compas/compass.dart';
 import 'package:weather_app/repositories/weather_forecast/model/all_weather_forecast.dart';
 
 class WindWidget extends StatelessWidget {
@@ -24,7 +24,13 @@ class WindWidget extends StatelessWidget {
               size: 20,
               color: Colors.white,
             ),
-            Text('Wind', style: TextStyle(fontSize: 16, color: Colors.white))
+            Text(
+              'Wind',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.white,
+              ),
+            )
           ],
         ),
         width: MediaQuery.of(context).size.width - 24,
@@ -36,30 +42,60 @@ class WindWidget extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const Text('Wind',
-                          style: TextStyle(fontSize: 16, color: Colors.white)),
-                      Text('${(allForecast.windKpH / 3.6).toInt()} mhp',
-                          style: TextStyle(fontSize: 16, color: Colors.white))
+                      const Text(
+                        'Wind',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        '${(allForecast.windKpH / 3.6).toInt()} mhp',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
                     ],
                   ),
                   Divider(),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const Text('Gust:',
-                          style: TextStyle(fontSize: 16, color: Colors.white)),
-                      Text('${(allForecast.gustKph / 3.6).toInt()} mph',
-                          style: TextStyle(fontSize: 16, color: Colors.white))
+                      const Text(
+                        'Gust:',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        '${(allForecast.gustKph / 3.6).toInt()} mph',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      )
                     ],
                   ),
                   Divider(),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const Text('Direction',
-                          style: TextStyle(fontSize: 16, color: Colors.white)),
-                      Text('${allForecast.windDegree}° ${allForecast.windDir}',
-                          style: TextStyle(fontSize: 16, color: Colors.white))
+                      const Text(
+                        'Direction',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        '${allForecast.windDegree}° ${allForecast.windDir}',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                        ),
+                      )
                     ],
                   ),
                 ],
@@ -67,7 +103,7 @@ class WindWidget extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: Compas(allForecast: allForecast),
+              child: Compass(allForecast: allForecast),
             )
           ],
         ),
